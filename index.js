a8857956343d31a92821d38944a5706fb8836a50
Christopher Wong <cw30355@gmail.com>
2023-01-04 14:10:03 -0500

    commit tree 191acc435bf7bd51a22b316692ab51ab81d26a12

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "Chris",
+    streetAddress: "1 Main Street"
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value){
+    return{
+        ...object,
+        [key]: value
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){
+    const deletedEmployee = {...employee}
+    delete deletedEmployee[key]
+    return deletedEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee 
+}
